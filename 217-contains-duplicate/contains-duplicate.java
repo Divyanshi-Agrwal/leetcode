5bb7659028672732192c3104fbf8500323ad6000
Divyanshi Agrwal <divyanshia472@gmail.com>
2026-07-01 16:57:54 +0530

    Time: 19 ms (33.18%) | Memory: 112.2 MB (5.72%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,31 +1,21 @@
-// class Solution {
-//     public boolean containsDuplicate(int[] nums) {
-       
-//         HashMap<Integer, Integer> hm = new HashMap <Integer, Integer>();
-//         for(int i = 0; i<nums.length; i++){
-//             if(hm.containsKey(nums[i])){
-                
-//                 return true;
-//             }
-//             else{
-//                 hm.put(nums[i],i);
-//             }
-//         }  
-//         return false;    
-//     }
-// }
-
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-
-        for (int num : nums) {
-            if (!set.add(num))
+       
+        HashMap<Integer, Integer> hm = new HashMap <Integer, Integer>();
+        for(int i = 0; i<nums.length; i++){
+            if(hm.containsKey(nums[i])){
+                
                 return true;
-        }
-        return false;
+            }
+            else{
+                hm.put(nums[i],i);
+            }
+        }  
+        return false;    
     }
 }
+
+
 
         
         
