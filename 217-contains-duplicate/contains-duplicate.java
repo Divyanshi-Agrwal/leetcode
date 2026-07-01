e5a238a473d6f7b714742e9fa7e4d02bcf010a02
Divyanshi Agrwal <divyanshia472@gmail.com>
2026-07-01 16:56:09 +0530

    Time: 13 ms (90.60%) | Memory: 108.7 MB (5.72%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,17 +1,29 @@
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+       
+//         HashMap<Integer, Integer> hm = new HashMap <Integer, Integer>();
+//         for(int i = 0; i<nums.length; i++){
+//             if(hm.containsKey(nums[i])){
+                
+//                 return true;
+//             }
+//             else{
+//                 hm.put(nums[i],i);
+//             }
+//         }  
+//         return false;    
+//     }
+// }
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-       
-        HashMap<Integer, Integer> hm = new HashMap <Integer, Integer>();
-        for(int i = 0; i<nums.length; i++){
-            if(hm.containsKey(nums[i])){
-                
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (!set.add(num))
                 return true;
-            }
-            else{
-                hm.put(nums[i],i);
-            }
-        }  
-        return false;    
+        }
+        return false;
     }
 }
 
