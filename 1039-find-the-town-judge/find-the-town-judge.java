21eaa76a0552bab8d4d12884cb9d40b6d286cd62
Divyanshi Agrwal <divyanshia472@gmail.com>
2026-07-28 23:24:07 +0530

    Time: 2 ms (99.67%) | Memory: 53.8 MB (34.50%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,19 +1,41 @@
+// class Solution {
+//     public int findJudge(int n, int[][] trust) {
+//         // int[] count = new int[n+1];
+//         // for(int[] t : trust){
+//         //     int a = t[0];
+//         //     int b = t[1];
+
+//         //     count[a]--;
+//         //     count[b]++;
+//         // }
+//         // for(int i = 1; i<=n; i++){
+//         //     if(count[i] == n-1){
+//         //         return i;
+//         //     }
+//         // }
+        
+//         }
+//         return -1;
+        
+//     }
+// }
+
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        int[] count = new int[n+1];
-        for(int[] t : trust){
-            int a = t[0];
-            int b = t[1];
+        int[] inDegree = new int[n + 1];
+        int[] outDegree = new int[n + 1];
 
-            count[a]--;
-            count[b]++;
+        for (int[] t : trust) {
+            outDegree[t[0]]++;
+            inDegree[t[1]]++;
         }
-        for(int i = 1; i<=n; i++){
-            if(count[i] == n-1){
+
+        for (int i = 1; i <= n; i++) {
+            if (inDegree[i] == n - 1 && outDegree[i] == 0) {
                 return i;
             }
         }
+
         return -1;
-        
     }
 }
