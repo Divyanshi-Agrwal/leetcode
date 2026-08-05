30c363e91be10b751b015ee4c711df1a80bccafb
Divyanshi Agrwal <divyanshia472@gmail.com>
2026-08-05 19:43:43 +0530

    Time: 1 ms (99.95%) | Memory: 77.3 MB (32.57%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,18 +1,21 @@
 class Solution {
     public int maxSubArray(int[] nums){
+        //kadens algorithm
         int maxSum = nums[0];
         int sum = 0;
         for(int i = 0; i<nums.length; i++){
             sum = sum += nums[i];
-        
-        if(sum > maxSum){
-            maxSum = sum;
-        }
-        if(sum < 0){
-            sum = 0;
-        }
+
+            if(sum > maxSum){
+                maxSum = sum;
+            }
+            if(sum < 0){
+                sum = 0;
+            }
+
         }
         return maxSum;
+     
     }
     
 }
